877e4bc3aca7d6c205dc0c2da55560867ad8db64
IFraimG <kulsasha55@gmail.com>
2024-11-10 15:32:34 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import basicSsl from '@vitejs/plugin-basic-ssl'
+// import basicSsl from '@vitejs/plugin-basic-ssl'
 
-import fs from "fs"
+// import fs from "fs"
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,17 +14,17 @@ export default defineConfig({
     vue(),
     vueJsx(),
     vueDevTools(),
-    basicSsl(),
+    // basicSsl(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  server: {
-    https: {
-      key: fs.readFileSync('./key.pem'),
-      cert: fs.readFileSync('./cert.pem')
-    }
-  }
+  // server: {
+  //   https: {
+  //     key: fs.readFileSync('./key.pem'),
+  //     cert: fs.readFileSync('./cert.pem')
+  //   }
+  // }
 })
